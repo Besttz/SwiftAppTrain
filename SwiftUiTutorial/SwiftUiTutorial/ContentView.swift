7d290cded046cee50e4843087fe2af8e95a58b86
Tommy <tommyzhangchina@gmail.com>
2020-06-27 21:23:04 +0800

    1.0 Create Project
    SwiftUI offical tutorial Landmarks App
    https://developer.apple.com/tutorials/swiftui/creating-and-combining-views

---
+++ SwiftUiTutorial/SwiftUiTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiTutorial
+//
+//  Created by ZhangTommy on 27/6/20.
+//  Copyright © 2020 Tommy Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Tommy!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
