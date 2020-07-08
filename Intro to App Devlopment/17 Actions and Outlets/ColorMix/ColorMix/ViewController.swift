99f5ba4216abb6d19776fc8366c51d9ea884e815
Tommy <tommyzhangchina@gmail.com>
2020-07-09 02:03:34 +0800

    2.17.1

---
+++ Intro to App Devlopment/17 Actions and Outlets/ColorMix/ColorMix/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by ZhangTommy on 9/7/20.
+//  Copyright © 2020 Tommy Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        colorView.backgroundColor = .black
+    }
+
+
+}
+
