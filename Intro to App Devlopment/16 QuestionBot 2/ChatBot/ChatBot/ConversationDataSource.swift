d1efb291280e39988005968707e8e6d40a6a4ff6
Tommy <tommyzhangchina@gmail.com>
2020-07-09 01:43:12 +0800

    2.16 Done
    Intro to App Dev Lesson 16

--- Intro to App Devlopment/16 QuestionBot 2/ChatBot/ChatBot/ConversationDataSource.swift
+++ Intro to App Devlopment/16 QuestionBot 2/ChatBot/ChatBot/ConversationDataSource.swift
@@ -2,21 +2,30 @@ import Foundation
 class ConversationDataSource {
     
     /// The number of Messages in the conversation
-    let messageCount = 0
+    var messageCount:Int {
+        return msgs.count
+    }
+//    var msgs = [Message]()
+    var msgs = [openingLine]
     
     /// Add a new question to the conversation
     func add(question: String) {
         print("Asked to add question: \(question)")
+        let msg = Message(date: Date(), text: question, type: .question)
+        msgs.append(msg)
+        
     }
     
     /// Add a new answer to the conversation
     func add(answer: String) {
         print("Asked to add answer: \(answer)")
+        let msg = Message(date: Date(), text: answer, type: .answer)
+        msgs.append(msg)
     }
     
     /// The Message at a specific point in the conversation
     func messageAt(index: Int) -> Message {
         print("Asking for message at index \(index)")
-        return Message(date: Date(), text: "Hello, world!", type: .answer)
+        return msgs[index]
     }
 }
