14b1c82141dffe92dbd52f4e6a9707e6bac7c2a9
Tommy <tommyzhangchina@gmail.com>
2020-07-10 01:45:40 +0800

    2.17.3

--- Intro to App Devlopment/17 Actions and Outlets/ColorMix/ColorMix/ViewController.swift
+++ Intro to App Devlopment/17 Actions and Outlets/ColorMix/ColorMix/ViewController.swift
@@ -9,21 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var rSwitch: UISwitch!
+    @IBOutlet weak var gSwitch: UISwitch!
+    @IBOutlet weak var bSwitch: UISwitch!
+    
     @IBAction func switchChanged(_ sender: UISwitch) {
-        if sender.isOn {
-            colorView.backgroundColor = .red
-        } else {
-            colorView.backgroundColor = .black
-        }
+        updateColor()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         colorView.backgroundColor = .black
     }
-
-
+    
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if rSwitch.isOn {
+            red = 1
+        }
+        if gSwitch.isOn {
+            green = 1
+        }
+        if bSwitch.isOn {
+            blue = 1
+        }
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+    
+    
 }
 
