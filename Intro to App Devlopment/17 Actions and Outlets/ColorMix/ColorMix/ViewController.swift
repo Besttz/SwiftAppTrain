7cbcd38e776d411bd7e17cbc77ec82643a8806f1
Tommy <tommyzhangchina@gmail.com>
2020-07-10 01:27:37 +0800

    2.17.2

--- Intro to App Devlopment/17 Actions and Outlets/ColorMix/ColorMix/ViewController.swift
+++ Intro to App Devlopment/17 Actions and Outlets/ColorMix/ColorMix/ViewController.swift
@@ -11,6 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            colorView.backgroundColor = .red
+        } else {
+            colorView.backgroundColor = .black
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
