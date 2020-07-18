fc4ad341079233708872d8ce0e58a959ff407ca2
Tommy <tommyzhangchina@gmail.com>
2020-07-18 15:21:15 +0800

    2.18.1

---
+++ Intro to App Devlopment/18 Adaptive User Interfaces/ElementQuiz/ElementQuiz/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by ZhangTommy on 17/7/20.
+//  Copyright © 2020 Tommy Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    
+    var currentElementIndex = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func gotoNextElement(_ sender: UIButton) {
+        currentElementIndex += 1
+        updateElement()
+    }
+    @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateElement()
+    }
+    
+    func updateElement() {
+        answerLabel.text="?"
+        let eleName = elementList[currentElementIndex]
+        let img = UIImage(named: eleName)
+        imageView.image = img
+    }
+    
+    
+}
+
