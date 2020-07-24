41cb1d392af703199bff335d35868b13f73104eb
Tommy <tommyzhangchina@gmail.com>
2020-07-24 21:59:29 +0800

    2.20.2

--- Intro to App Devlopment/20 Final Project/RPS/RPS/ViewController.swift
+++ Intro to App Devlopment/20 Final Project/RPS/RPS/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var user:Sign?
     
     @IBOutlet weak var icon: UILabel!
     @IBOutlet weak var maiTitle: UILabel!
@@ -17,17 +18,71 @@ class ViewController: UIViewController {
     @IBOutlet weak var sIcon: UIButton!
     @IBOutlet weak var pIcon: UIButton!
     @IBOutlet weak var paButton: UIButton!
+    //    @IBOutlet var background: UIView!
     
     @IBAction func tapR(_ sender: Any) {
+        user = .rock
+        action()
     }
     @IBAction func tapS(_ sender: Any) {
+        user = .scissors
+        action()
     }
     @IBAction func taP(_ sender: Any) {
+        user = .paper
+        action()
     }
     @IBAction func paButton(_ sender: Any) {
+        rIcon.isHidden = false
+        pIcon.isHidden = false
+        sIcon.isHidden = false
+        pIcon.isEnabled = true
+        rIcon.isEnabled = true
+        sIcon.isEnabled = true
+        maiTitle.text = "Hello!"
+        icon.text = "🤖"
+         paButton.isEnabled = false
+        view.backgroundColor = UIColor.white
     }
     
-    func action()  {
+    func action() {
+        paButton.isEnabled = true
+        if user == nil {
+            return
+        }
+        let com = randomSign()
+        let state = user?.compare(com)
+        
+        icon.text = com.emoji
+        switch user {
+        case .paper:
+            rIcon.isHidden = true
+            sIcon.isHidden = true
+            pIcon.isEnabled = false
+        case .rock:
+            pIcon.isHidden = true
+            sIcon.isHidden = true
+            rIcon.isEnabled = false
+        case .scissors:
+            rIcon.isHidden = true
+            pIcon.isHidden = true
+            sIcon.isEnabled = false
+        default:
+            return
+        }
+        switch state {
+        case .draw:
+            maiTitle.text = "Come on and Try Again!"
+            view.backgroundColor = UIColor.gray
+        case .win:
+            maiTitle.text = "Yeaa!"
+            view.backgroundColor = UIColor.green
+        case .lose:
+            maiTitle.text = "Opps!"
+            view.backgroundColor = UIColor.red
+        default:
+            return
+        }
         
     }
     
@@ -36,7 +91,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         paButton.isEnabled = false
     }
-
-
+    
+    
 }
 
