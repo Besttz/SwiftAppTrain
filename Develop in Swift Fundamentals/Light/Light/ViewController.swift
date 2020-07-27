a917ac7cb8fddd4254ec0d7891821ac0cb511c2a
Tommy <tommyzhangchina@gmail.com>
2020-07-27 15:36:01 +0800

    3.1.2

--- Develop in Swift Fundamentals/Light/Light/ViewController.swift
+++ Develop in Swift Fundamentals/Light/Light/ViewController.swift
@@ -11,16 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     var lightOn = true
-    @IBOutlet weak var lightButton: UIButton!
+    
     
     fileprivate func UpdateUI() {
-        if lightOn {
-            view.backgroundColor = .white
-            lightButton.setTitle("Off", for: .normal)
-        } else {
-            view.backgroundColor = .black
-            lightButton.setTitle("On", for: .normal)
-        }
+        view.backgroundColor = lightOn ? .white : .black
     }
     
     @IBAction func buttonPressed(_ sender: Any) {
