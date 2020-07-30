4b6bd8053e9f530a7b59e227b23a2062e36d48d4
Tommy <tommyzhangchina@gmail.com>
2020-07-30 23:25:37 +0800

    3 3.6 Lab Done

--- Develop in Swift Fundamentals/Login/Login/ViewController.swift
+++ Develop in Swift Fundamentals/Login/Login/ViewController.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var fUButton: UIButton!
+    @IBOutlet weak var fPButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = name.text
+        guard let sender = sender as? UIButton else {
+            return
+        }
+        if sender == fUButton {
+            segue.destination.navigationItem.title = "F  U"
+        } else if sender == fPButton {
+            segue.destination.navigationItem.title = "F  P"
+        } else {
+            segue.destination.navigationItem.title = name.text
+        }
     }
-
+    
+    @IBAction func fUTapped(_ sender: Any) {
+        performSegue(withIdentifier: "submit", sender: sender)
+    }
+    @IBAction func fPTapped(_ sender: Any) {
+        performSegue(withIdentifier: "submit", sender: sender)
+    }
+    
 }
 
