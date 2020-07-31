b9713af843a14823ad0e57293143f711a6895ef4
Tommy <tommyzhangchina@gmail.com>
2020-07-31 23:25:50 +0800

    3 3.7 Tap R&Orange

---
+++ Develop in Swift Fundamentals/Rainbow/Rainbow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by ZhangTommy on 31/7/20.
+//  Copyright © 2020 Tommy Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
