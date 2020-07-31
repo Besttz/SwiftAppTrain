3de1579564edb16fd854dc8eafebe9ce8350d551
Tommy <tommyzhangchina@gmail.com>
2020-07-31 23:42:50 +0800

    3 3.7.5 Tab Lab Done

---
+++ Develop in Swift Fundamentals/Rainbow/Rainbow/OViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by ZhangTommy on 31/7/20.
+//  Copyright © 2020 Tommy Zhang. All rights reserved.
+//
+
+import UIKit
+
+class OViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
