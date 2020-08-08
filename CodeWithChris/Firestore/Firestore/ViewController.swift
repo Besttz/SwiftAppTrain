135de0147c38ae9a83fdd3f5790d6dc43aff442b
Tommy <tommyzhangchina@gmail.com>
2020-08-08 20:59:31 +0800

    4 2.1.8 Update Objects

--- CodeWithChris/Firestore/Firestore/ViewController.swift
+++ CodeWithChris/Firestore/Firestore/ViewController.swift
@@ -19,14 +19,21 @@ class ViewController: UIViewController {
         
         let db = Firestore.firestore()
         
-        // Create Collection/Documents
         
+        /* // Create Collection/Documents
+         
+        db.collection("game").addDocument(data: ["name" : "Super Mario World","platform" : "Super Nintendo","genre" : "Platformer"])
+         
         db.collection("consoles").addDocument(data: ["name" : "Super Mario World","platform" : "Super Nintendo","genre" : "Platformer"])
         
         db.collection("consoles").document("snes").setData(["name" : "Super Nintendo Entertainment System","company" : "Nintendo"])
         
         db.collection("consoles").document("nes").setData(["name" : "Nintendo Entertainment System","company" : "Nintendo"])
+        */
         
+        // Update Objects
+        db.collection("game").document("PdcaZACx2wSM4OKar2JE").setData([ "platform":"SNES", "rating" : "E", "year" : "1990"], merge: true)
+        db.collection("consoles").document("snes").setData([ "name":"Super NES", "units" : "49 million", "year" : "1990"], merge: true)
     }
 
 
