0e60149c12e209238dc0342302027e8948f6a93b
Tommy <tommyzhangchina@gmail.com>
2020-08-08 20:52:41 +0800

    4 2.1.7 Create Collection & Documents

--- CodeWithChris/Firestore/Firestore/ViewController.swift
+++ CodeWithChris/Firestore/Firestore/ViewController.swift
@@ -19,9 +19,13 @@ class ViewController: UIViewController {
         
         let db = Firestore.firestore()
         
-//        db.collection("game").addDocument(data: ["name" : "Super Mario World","platform" : "Super Nintendo","genre" : "Platformer"])
+        // Create Collection/Documents
         
-//        db.collection("game").document("tetris").setData(["name" : "Tetris","platform" : "Nintendo","genre" : "Puzzle"])
+        db.collection("consoles").addDocument(data: ["name" : "Super Mario World","platform" : "Super Nintendo","genre" : "Platformer"])
+        
+        db.collection("consoles").document("snes").setData(["name" : "Super Nintendo Entertainment System","company" : "Nintendo"])
+        
+        db.collection("consoles").document("nes").setData(["name" : "Nintendo Entertainment System","company" : "Nintendo"])
         
     }
 
