17b54b3dd16ca876d1e59216812dba88609ee7fe
Tommy <tommyzhangchina@gmail.com>
2020-08-08 21:05:49 +0800

    4 2.1.9 Delete Objects

--- CodeWithChris/Firestore/Firestore/ViewController.swift
+++ CodeWithChris/Firestore/Firestore/ViewController.swift
@@ -12,7 +12,7 @@ import FirebaseFirestore
 //import FirebaseCore
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,20 +22,25 @@ class ViewController: UIViewController {
         
         /* // Create Collection/Documents
          
-        db.collection("game").addDocument(data: ["name" : "Super Mario World","platform" : "Super Nintendo","genre" : "Platformer"])
+         db.collection("game").addDocument(data: ["name" : "Super Mario World","platform" : "Super Nintendo","genre" : "Platformer"])
          
-        db.collection("consoles").addDocument(data: ["name" : "Super Mario World","platform" : "Super Nintendo","genre" : "Platformer"])
+         db.collection("consoles").addDocument(data: ["name" : "Super Mario World","platform" : "Super Nintendo","genre" : "Platformer"])
+         
+         db.collection("consoles").document("snes").setData(["name" : "Super Nintendo Entertainment System","company" : "Nintendo"])
+         
+         db.collection("consoles").document("nes").setData(["name" : "Nintendo Entertainment System","company" : "Nintendo"])
+         */
         
-        db.collection("consoles").document("snes").setData(["name" : "Super Nintendo Entertainment System","company" : "Nintendo"])
+        /* // Update Objects
+         db.collection("game").document("PdcaZACx2wSM4OKar2JE").setData([ "platform":"SNES", "rating" : "E", "year" : "1990"], merge: true)
+         db.collection("consoles").document("snes").setData([ "name":"Super NES", "units" : "49 million", "year" : "1990"], merge: true)
+         */
         
-        db.collection("consoles").document("nes").setData(["name" : "Nintendo Entertainment System","company" : "Nintendo"])
-        */
-        
-        // Update Objects
-        db.collection("game").document("PdcaZACx2wSM4OKar2JE").setData([ "platform":"SNES", "rating" : "E", "year" : "1990"], merge: true)
-        db.collection("consoles").document("snes").setData([ "name":"Super NES", "units" : "49 million", "year" : "1990"], merge: true)
+        // Delete Object
+        db.collection("game").document("PdcaZACx2wSM4OKar2JE").setData([ "genre":FieldValue.delete()], merge: true)
+        db.collection("consoles").document("snes").delete()
     }
-
-
+    
+    
 }
 
