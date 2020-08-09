2190fd9c1a00c0c791efd9f3ba4c1cb5deb93936
Tommy <tommyzhangchina@gmail.com>
2020-08-09 21:08:06 +0800

    4.2.2.1 Basic UI and Struct

---
+++ CodeWithChris/Journal/Journal/Model/Note.swift
@@ -0,0 +1,20 @@
+//
+//  Note.swift
+//  Journal
+//
+//  Created by ZhangTommy on 9/8/20.
+//  Copyright © 2020 Tommy Zhang. All rights reserved.
+//
+
+import Foundation
+
+struct Note {
+    
+    var docId:String
+    var title:String
+    var body:String
+    var isStarred:Bool
+    var createdAt:Date
+    var lastUpdatedAt:Date
+    
+}
--- CodeWithChris/Journal/Journal/View/ViewController.swift
+++ CodeWithChris/Journal/Journal/View/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    private var notes = [Note]()
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
